63a2e29059d01f27418d013208b8ef6877806b12
antonin 31 <antonin.klopp-tosser@grenoble-inp.org>
2018-09-18 13:27:36 +0200

    Initialisation of the repository

---
+++ WeaponInfo.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using DemoInfo;
+using System.Linq;
+
+public class WeaponInfo
+{
+    /// <summary>
+    /// Get the infos about the max Ammo in the Weapons. 
+    /// </summary>
+    /// <param name="weapon"></param>
+    /// <returns></returns>
+    public static int WeaponAmmoMagazine(EquipmentElement weapon) {
+        switch (weapon) {
+            case EquipmentElement.Unknown:
+                return 0; 
+            case EquipmentElement.P2000:
+                break;
+            case EquipmentElement.Glock:
+                break;
+            case EquipmentElement.P250:
+                break;
+            case EquipmentElement.Deagle:
+                break;
+            case EquipmentElement.FiveSeven:
+                break;
+            case EquipmentElement.DualBarettas:
+                break;
+            case EquipmentElement.Tec9:
+                break;
+            case EquipmentElement.CZ:
+                break;
+            case EquipmentElement.USP:
+                break;
+            case EquipmentElement.Revolver:
+                break;
+            case EquipmentElement.MP7:
+                break;
+            case EquipmentElement.MP9:
+                break;
+            case EquipmentElement.Bizon:
+                break;
+            case EquipmentElement.Mac10:
+                break;
+            case EquipmentElement.UMP:
+                break;
+            case EquipmentElement.P90:
+                break;
+            case EquipmentElement.SawedOff:
+                break;
+            case EquipmentElement.Nova:
+                break;
+            case EquipmentElement.Swag7:
+                break;
+            case EquipmentElement.XM1014:
+                break;
+            case EquipmentElement.M249:
+                break;
+            case EquipmentElement.Negev:
+                break;
+            case EquipmentElement.Gallil:
+                break;
+            case EquipmentElement.Famas:
+                break;
+            case EquipmentElement.AK47:
+                break;
+            case EquipmentElement.M4A4:
+                break;
+            case EquipmentElement.M4A1:
+                break;
+            case EquipmentElement.Scout:
+                break;
+            case EquipmentElement.SG556:
+                break;
+            case EquipmentElement.AUG:
+                break;
+            case EquipmentElement.AWP:
+                break;
+            case EquipmentElement.Scar20:
+                break;
+            case EquipmentElement.G3SG1:
+                break;
+            case EquipmentElement.Zeus:
+                break;
+            case EquipmentElement.Kevlar:
+                break;
+            case EquipmentElement.Helmet:
+                break;
+            case EquipmentElement.Bomb:
+                break;
+            case EquipmentElement.Knife:
+                break;
+            case EquipmentElement.DefuseKit:
+                break;
+            case EquipmentElement.World:
+                break;
+            case EquipmentElement.Decoy:
+                break;
+            case EquipmentElement.Molotov:
+                break;
+            case EquipmentElement.Incendiary:
+                break;
+            case EquipmentElement.Flash:
+                break;
+            case EquipmentElement.Smoke:
+                break;
+            case EquipmentElement.HE:
+                break;
+        }
+
+        return 0;
+    }
+}
