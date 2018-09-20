7316935da166d29815a9ca494cde725573683c9f
antonin 31 <antonin.klopp-tosser@grenoble-inp.org>
2018-09-20 21:08:22 +0200

    Added JSON Parser for maps

--- GenerateHeatMap.cs
+++ GenerateHeatMap.cs
@@ -4,11 +4,16 @@ using DemoInfo;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Collections;
-using System.Collections.Generic; 
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace CS_GO_Analysis {
     public class GenerateHeatMaps {
         public static void GenerateMap(List<Death> listDeaths, int roundNumber=0) {
+
+            Map_JSON map = JsonConvert.DeserializeObject<Map_JSON>(File.ReadAllText("Maps_json/de_mirage.json"));
+            Console.WriteLine("JSON {0}", map.pos_x); 
+
             Bitmap bitmap = new Bitmap("Maps/de_mirage_radar.png");
             Graphics g = Graphics.FromImage(bitmap);
 
---
+++ Map_JSON.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace CS_GO_Analysis {
+    public class Map_JSON {
+
+        string material; 
+
+        /// <summary>
+        /// Upper left corner X position
+        /// </summary>
+        public int pos_x;
+
+        /// <summary>
+        /// Upper left corner Y position
+        /// </summary>
+        public int pos_y;
+
+        public float scale;
+
+        public float CTSpawn_x;
+        public float CTSpawn_y;
+
+        public float TSpawn_x;
+        public float TSpawn_y;
+
+        public float bombA_x;
+        public float bombA_y;
+
+        public float bombB_x;
+        public float bombB_y;
+
+        public Map_JSON() {
+
+        }
+    }
+
+}
