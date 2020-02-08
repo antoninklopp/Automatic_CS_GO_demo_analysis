82cec5762f0276f7ec3a63ee21a3798da23d71f2
Antoine Levotre <antoine.levotre@grenoble-inp.org>
2020-02-08 20:31:22 +0100

    Divided the heatmaps for Kills, Deaths, Positions and Holdings into CT side and T side sub-folders.

--- Maps_programs/GenerateHeatMap.cs
+++ Maps_programs/GenerateHeatMap.cs
@@ -46,49 +46,57 @@ namespace CS_GO_Analysis.Maps {
         /// Generate a Map of all the deaths of a player
         /// </summary>
         public static void GenerateDeathsPlayer(Player p, string mapName, string folder= "Test_images") {
-            Bitmap bitmap = new Bitmap("Maps/" + mapName + "_radar.png");
-            Graphics g = Graphics.FromImage(bitmap);
+            Bitmap bitmap_CT = new Bitmap("Maps/" + mapName + "_radar.png");
+            Bitmap bitmap_T = new Bitmap("Maps/" + mapName + "_radar.png");
+            Graphics g_CT = Graphics.FromImage(bitmap_CT);
+            Graphics g_T = Graphics.FromImage(bitmap_T);
 
             List<Death> listDeaths = p.AllDeaths;
             string playerName = p.Name; 
 
             foreach (Death d in listDeaths) {
                 if (d.T == Team.CounterTerrorist) {
-                    g.DrawEllipse(Pens.DarkBlue, new Rectangle((int)d.Position.X, (int)d.Position.Y, 5, 5));
+                    g_CT.DrawEllipse(Pens.DarkBlue, new Rectangle((int)d.Position.X, (int)d.Position.Y, 5, 5));
                 } else {
-                    g.DrawEllipse(Pens.Yellow, new Rectangle((int)d.Position.X, (int)d.Position.Y, 5, 5));
+                    g_T.DrawEllipse(Pens.Yellow, new Rectangle((int)d.Position.X, (int)d.Position.Y, 5, 5));
                 }
             }
             
             if (!Directory.Exists(folder + "/Deaths")) {
-                Directory.CreateDirectory(folder + "/Deaths"); 
+                Directory.CreateDirectory(folder + "/Deaths/CT"); 
+                Directory.CreateDirectory(folder + "/Deaths/T"); 
             }
-            bitmap.Save(folder + "/Deaths/" + mapName + "_" + playerName + ".png", ImageFormat.Png);
+            bitmap_CT.Save(folder + "/Deaths/CT/" + mapName + "_" + playerName + ".png", ImageFormat.Png);
+            bitmap_T.Save(folder + "/Deaths/T/" + mapName + "_" + playerName + ".png", ImageFormat.Png);
         }
 
         /// <summary>
         /// Generate a Map of all the deaths of a player
         /// </summary>
         public static void GenerateKillsPlayer(Player p, string mapName, string folder="Test_images") {
-            Bitmap bitmap = new Bitmap("Maps/" + mapName + "_radar.png");
-            Graphics g = Graphics.FromImage(bitmap);
+            Bitmap bitmap_CT = new Bitmap("Maps/" + mapName + "_radar.png");
+            Bitmap bitmap_T = new Bitmap("Maps/" + mapName + "_radar.png");
+            Graphics g_CT = Graphics.FromImage(bitmap_CT);
+            Graphics g_T = Graphics.FromImage(bitmap_T);
 
             List<Kill> listKills = p.AllKills;
             string playerName = p.Name;
 
             foreach (Kill k in listKills) {
                 if (k.T == Team.CounterTerrorist) {
-                    g.DrawEllipse(Pens.DarkBlue, new Rectangle((int)k.Position.X, (int)k.Position.Y, 5, 5));
+                    g_CT.DrawEllipse(Pens.DarkBlue, new Rectangle((int)k.Position.X, (int)k.Position.Y, 5, 5));
                 }
                 else {
-                    g.DrawEllipse(Pens.Yellow, new Rectangle((int)k.Position.X, (int)k.Position.Y, 5, 5));
+                    g_T.DrawEllipse(Pens.Yellow, new Rectangle((int)k.Position.X, (int)k.Position.Y, 5, 5));
                 }
             }
 
             if (!Directory.Exists(folder + "/Kills")) {
-                Directory.CreateDirectory(folder + "/Kills");
+                Directory.CreateDirectory(folder + "/Kills/CT");
+                Directory.CreateDirectory(folder + "/Kills/T");
             }
-            bitmap.Save(folder + "/Kills/" + mapName + "_" + playerName + ".png", ImageFormat.Png);
+            bitmap_CT.Save(folder + "/Kills/CT/" + mapName + "_" + playerName + ".png", ImageFormat.Png);
+            bitmap_T.Save(folder + "/Kills/T/" + mapName + "_" + playerName + ".png", ImageFormat.Png);
         }
 
         public static void GenerateHeatpMapPosition(Player p, string mapName, Team t, string folder="Test_images") {
@@ -124,10 +132,18 @@ namespace CS_GO_Analysis.Maps {
             }
 
             if (!Directory.Exists(folder + "/Position")) {
-                Directory.CreateDirectory(folder + "/Position");
+                Directory.CreateDirectory(folder + "/Position/CT");
+                Directory.CreateDirectory(folder + "/Position/T");
             }
 
-            bitmap.Save(folder + "/Position/HeatMap_" + mapName + "_" + p.Name + "_" + t + ".png", ImageFormat.Png);
+            if (t == Team.CounterTerrorist)
+            {
+                bitmap.Save(folder + "/Position/CT/HeatMap_" + mapName + "_" + p.Name + ".png", ImageFormat.Png);
+            }
+            else
+            {
+                bitmap.Save(folder + "/Position/T/HeatMap_" + mapName + "_" + p.Name + ".png", ImageFormat.Png);
+            }
         }
 
         public static void GenerateHeatpMapPositionHolding(Player p, string mapName, Team t, string folder = "Test_images") {
@@ -164,10 +180,18 @@ namespace CS_GO_Analysis.Maps {
             }
 
             if (!Directory.Exists(folder + "/Holding")) {
-                Directory.CreateDirectory(folder + "/Holding");
+                Directory.CreateDirectory(folder + "/Holding/CT");
+                Directory.CreateDirectory(folder + "/Holding/T");
             }
 
-            bitmap.Save(folder + "/Holding/HeatMap_" + mapName + "_" + p.Name + "_" + t + ".png", ImageFormat.Png);
+            if (t == Team.CounterTerrorist)
+            {
+                bitmap.Save(folder + "/Holding/CT/HeatMap_" + mapName + "_" + p.Name + ".png", ImageFormat.Png);
+            }
+            else
+            {
+                bitmap.Save(folder + "/Holding/T/HeatMap_" + mapName + "_" + p.Name + ".png", ImageFormat.Png);
+            }
         }
     }
 }
